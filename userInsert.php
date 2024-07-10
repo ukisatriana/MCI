90b8b719cd4c7c697fab3a9a377386873cd00e09
ukisatriana <ukisatriana@gmail.com>
2024-07-10 09:11:33 +0700

    edit insert

--- userInsert.php
+++ userInsert.php
@@ -7,8 +7,9 @@ $section = $_POST['section'];
 $division = $_POST['division'];
 $test_type = $_POST['test_type'];
 $score = $_POST['score'];
+$created_on = date('Y-m-d H:i:s'); // '2021-08-01 12:00:00
 
-$sql = "INSERT INTO user (nik, nama, section, division, test_type, score) VALUES ('$nik', '$nama', '$section', '$division', '$test_type', '$score')";
+$sql = "INSERT INTO user (nik, nama, section, division, test_type, score, created_on) VALUES ('$nik', '$nama', '$section', '$division', '$test_type', '$score', '$created_on')";
 $result = mysqli_query($conn, $sql);
 
 ?>
