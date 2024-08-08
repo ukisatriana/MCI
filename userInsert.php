2f948ad9abfddac8497e972fd40e49ce19590520
ukisatriana <ukisatriana@gmail.com>
2024-08-08 08:44:45 +0700

    update

--- userInsert.php
+++ userInsert.php
@@ -12,6 +12,12 @@ $created_on = date('Y-m-d H:i:s'); // '2021-08-01 12:00:00
 $sql = "INSERT INTO historical_test (nik, nama, section, division, test_type, score, created_on) VALUES ('$nik', '$nama', '$section', '$division', '$test_type', '$score', '$created_on')";
 $result = mysqli_query($conn, $sql);
 
+if ($result) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
 
 ?>
 
